7e054cd80788630a5435c3ac4307da22917b3b6e
Mohammed Idrees Ahmed <37972473+Idrees533@users.noreply.github.com>
2022-04-14 17:53:18 +0530

    initial comment

---
+++ math.py
@@ -0,0 +1,16 @@
+#Adding header value
+def add(x,y):
+pass
+#adding the substract implemtn
+def substract(x,y):
+return x-y;
+#mulitply
+def multiply(x,y)
+return x * y;  
+#Divide functn
+def divide(x,y)
+return x/y;   #bug 789
+
+#Square
+def square(x)
+pass
